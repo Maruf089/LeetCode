b690d3716d1bea73c4745baa560fd716b8366156
Maruf <maruf.cuet.cse16@gmail.com>
2023-03-08 19:59:43 +0600

    added new file

---
+++ 875 Koko Eating Bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long minEatingSpeed(vector<int>& piles, int h) {
+        long ans = INT_MAX;
+        long low = 1 , high = 1000000000;
+        while(low <= high)
+        {
+            long mid = (low + high) >> 1;
+            long sum = 0 ;
+            for(auto it : piles)
+            {
+                sum += (it / mid) + (it % mid == 0 ? 0 : 1);
+            }
+            if(sum <= h)
+              {
+                  if(mid < ans) ans = mid;
+                  high = mid - 1;
+              }
+            else low = mid + 1;
+
+        }
+        return ans;
+
+    }
+};
