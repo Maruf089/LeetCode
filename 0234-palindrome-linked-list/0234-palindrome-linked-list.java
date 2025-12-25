283c58ec4cebaa3a82b1521c192defce215065db
Md Maruf Hasan <maruf.cuet.cse16@gmail.com>
2025-12-25 09:45:44 +0600

    Time: 4 ms (81.73%), Space: 94.3 MB (71.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+    public boolean isPalindromeUsingList(ListNode head) {
         List<Integer>lst = new ArrayList<>();
         ListNode curr = head;
         while(curr != null){
@@ -34,4 +34,67 @@ class Solution {
         return palin;
         
     }
+
+     public boolean isPalindromeUsingStack(ListNode head) {
+        Stack<Integer> st = new Stack<>();
+        ListNode curr = head;
+        while(curr != null){
+            st.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        boolean palin = true;
+        while(curr!=null){
+            Integer val = st.pop();
+            if(val != curr.val)
+            {
+                palin = false;
+                break;
+            }
+            curr = curr.next;
+        }
+        return palin;
+     }
+
+     public ListNode reverseNodes(ListNode head){
+        ListNode rev = null , tmp = null;
+        ListNode curr = head;
+        while(curr!=null){
+            tmp = curr.next;
+            curr.next = rev;
+            rev = curr;
+            curr = tmp;
+        }
+        return rev;
+     }
+
+     public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null)
+            return true;
+        ListNode slow = head , fast = head;
+
+        // find middle, slow will be at middle
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // If odd length, skip the middle node
+        if(fast!=null)
+            slow = slow.next;
+
+        ListNode secondHalfReversed = reverseNodes(slow);
+
+        ListNode firsthalf = head;
+        while(secondHalfReversed!=null){
+            if(firsthalf.val != secondHalfReversed.val)
+            {
+                return false;
+            }
+            firsthalf = firsthalf.next;
+            secondHalfReversed = secondHalfReversed.next;
+        }
+        return true;
+
+     }
 }
