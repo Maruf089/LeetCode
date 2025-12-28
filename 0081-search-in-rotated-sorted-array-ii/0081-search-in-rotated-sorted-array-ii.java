5e87622b8e7bad04e2885061f893362610e22d69
Md Maruf Hasan <maruf.cuet.cse16@gmail.com>
2025-12-28 14:50:02 +0600

    Time: 0 ms (100%), Space: 45 MB (57.5%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0 , right = nums.length - 1;
+
+        while(left<=right){
+
+            int mid = (left+right)/2;
+
+            if(target==nums[mid])
+                return true;
+            
+            if(nums[mid]==nums[left]){
+                left++;
+                continue;
+            }
+
+            if(nums[left] <= nums[mid]){
+                // left side sorted
+                if(nums[left]<= target && nums[mid]>=target)
+                    right = mid-1;
+                else left = mid + 1;
+            }
+            else{
+                // right side sorted
+                if(nums[mid]<= target && nums[right]>=target)
+                    left = mid + 1;
+                else right = mid-1;
+            }
+            
+        }
+        return false;
+        
+    }
+}
