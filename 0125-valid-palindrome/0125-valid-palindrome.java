6b75808be42118d2e0ea32f5437de2f358bca0c5
Md Maruf Hasan <maruf.cuet.cse16@gmail.com>
2025-12-31 17:31:34 +0600

    Time: 3 ms (64.35%), Space: 44.1 MB (85.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,35 @@
 class Solution {
+
     public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            Character l = s.charAt(left);
+            Character r = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(l)) {
+                left++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(r)) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(l) == Character.toLowerCase(r)) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindromeRegex(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int left = 0;
         int right = s.length() - 1;
