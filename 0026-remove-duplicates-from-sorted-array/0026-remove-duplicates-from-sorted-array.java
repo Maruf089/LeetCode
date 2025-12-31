9018aa95d377f203c5103698cd3c69fe8a0ce61b
Md Maruf Hasan <maruf.cuet.cse16@gmail.com>
2025-12-31 19:31:17 +0600

    Time: 1 ms (73.96%), Space: 46.8 MB (20.12%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+        int i = 1 , j = 1;
+        while(j<nums.length){
+            if(nums[j] != nums[i-1]){
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
