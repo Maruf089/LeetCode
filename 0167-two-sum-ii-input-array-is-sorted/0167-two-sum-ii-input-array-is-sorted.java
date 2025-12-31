7d8640bdf370551e81decb8121191ff3ee77a391
Md Maruf Hasan <maruf.cuet.cse16@gmail.com>
2025-12-31 17:11:28 +0600

    Time: 2 ms (96.32%), Space: 48.3 MB (78.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSumWithTwoLoop(int[] numbers, int target) {
         for(int i=0;i<numbers.length;i++){
             for(int j = i+1;j<numbers.length;j++){
                 int total = numbers[i] + numbers[j];
@@ -9,6 +9,24 @@ class Solution {
                 else if (total > target) break;
             }
         }
-        return new int[]{-1, -1}; // If no solution is found
+        return new int[]{-1, -1}; 
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int total = numbers[left] + numbers[right];
+
+            if (total == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (total > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[]{-1, -1}; // If no solution is found        
     }
 }
